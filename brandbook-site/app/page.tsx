b5abe57124d07b8fd7cd3699abe9f90ed158e87f
klkmoraa <crisdlm302@gmail.com>
2026-09-08 10:39:59 -0600

    brandbook: open hero on axial instead of red moment

--- brandbook-site/app/page.tsx
+++ brandbook-site/app/page.tsx
@@ -48,7 +48,7 @@ export default function Brandbook() {
   const motionMode: MotionMode =
     motionChoice ?? (prefersCalm ? 'calma' : 'activo');
   const [activeSection, setActiveSection] = useState<SectionId>('norte');
-  const [activeSignal, setActiveSignal] = useState<SignalId>('moment');
+  const [activeSignal, setActiveSignal] = useState<SignalId>('axial');
   const [copiedValue, setCopiedValue] = useState('');
   const [copiedLabel, setCopiedLabel] = useState('');
   const [copyFailed, setCopyFailed] = useState(false);
